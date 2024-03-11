16735c7455291b69b75c90fa6eebf15aee964ede
Jennifer Uraga Lopez <jenny@LIB-1FL-MAC14.lehman.edu>
2024-03-11 15:24:04 -0400

    Implement task rendering

--- script.js
+++ script.js
@@ -33,10 +33,23 @@ function handleSubmission(event) {
         tasks.push({ name: taskName, description: taskDescription, deadline: taskDeadline })
     render();
 }
+
+
 // Function to render tasks in the table
 function render() {
     // TODO: Use array methods to create a new table row of data for each item in the arr
+    taskTable.innerHTML = tasks.map(task => `
+      <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+      </tr>
+    `).join('');
 }
+
+
 // Function to initialize the table
 function init() {
     taskTable.innerHTML = ''; // Clear the table
